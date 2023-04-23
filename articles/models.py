63cd8216f125ce8dc84ceea93a68a4dc833afcd9
Young-Oh <gzf0547@gmail.com>
2023-04-24 03:50:09 +0900

    serializer POST method

--- articles/models.py
+++ articles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from articles.models import Article
@@ -6,10 +7,18 @@ from articles.serializers import ArticleSerializer
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def index(request):
-    articles = Article.objects.all()
-    article = articles[0]
-    serializer = ArticleSerializer(article) #단일
-    serializer = ArticleSerializer(articles, many=True) #다중
-    return Response(serializer.data)
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        article = articles[0]
+        serializer = ArticleSerializer(article) #단일
+        serializer = ArticleSerializer(articles, many=True) #다중
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(): #유효성 점검
+            serializer.save() #DB에 저장
+            return Response(serializer.data, status=status.HTTP_201_CREATED) #생성성공
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #요청오류
