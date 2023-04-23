f6805b40df268964f1f35cbbb0d676e251804dd7
Young-Oh <gzf0547@gmail.com>
2023-04-24 01:33:21 +0900

    add article app

--- DRF/settings.py
+++ DRF/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework', 
+    'rest_framework',
+    'articles',
     
     'django.contrib.admin',
     'django.contrib.auth',
