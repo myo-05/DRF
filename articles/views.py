00c5166ba4e87cdc055f297e137c90580ac26cf7
Young-Oh <gzf0547@gmail.com>
2023-04-24 02:55:55 +0900

    why need to use serializer? auto transfer data form to JSON!

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from articles.models import Article
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def index(request):
+    articles = Article.objects.all()
+    article = articles[0]
+    article_data={
+        "title":article.title,
+        "content":article.content,
+        "created_at":article.created_at,
+        "updated_at":article.updated_at,
+    }
+    
+    return Response(article_data)
