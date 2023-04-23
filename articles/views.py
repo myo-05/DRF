d19f7c78a53a794cdf4b8d48f7e889f3e982f4ae
Young-Oh <gzf0547@gmail.com>
2023-04-24 04:03:08 +0900

    detail(target) GET method

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from articles.models import Article
 from articles.serializers import ArticleSerializer
 
@@ -22,3 +23,11 @@ def index(request):
             serializer.save() #DB에 저장
             return Response(serializer.data, status=status.HTTP_201_CREATED) #생성성공
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #요청오류
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_view(request,article_id):
+    if request.method == 'GET':
+        # article = Article.objects.get(id=article_id) # 작동은 하나 id값이 유효하지 않으면 에러
+        article = get_object_or_404(Article,id=article_id) # 조회 에러처리를 위해 주로 사용!
+        serializer = ArticleSerializer(article)
+    return Response(serializer.data)
